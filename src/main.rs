deba005cf4bfda7777f56b7c434c942d8f6ed0b0
tkeiyama <thomas18thx@gmail.com>
2021-05-01 11:57:18 +0900

    Overwrite start attribute

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-fn main() {}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
